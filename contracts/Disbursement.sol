81581c6179f5abce89b696ea9b35960acdadcb0a
PunkUnknown <PunkUnknown@tutanota.com>
2021-01-26 14:54:20 +0500

    simplified

--- contracts/Disbursement.sol
+++ contracts/Disbursement.sol
@@ -13,14 +13,11 @@ contract Disbursement is Ownable {
     IERC20 public debase = IERC20(0x9248c485b0B80f76DA451f167A8db30F33C70907);
     address public policy = 0x989Edd2e87B1706AB25b2E8d9D9480DE3Cc383eD;
 
-    uint256 internal constant MAX_SUPPLY = ~uint128(0); // (2^128) - 1
-
     address public claimant;
-    uint256 public claimAmount;
     uint256 public claimPercentage;
 
-    function setClaimAmount(uint256 claimAmount_) external onlyOwner {
-        claimAmount = claimAmount_;
+    function setClaimPercentage(uint256 claimPercentage_) external onlyOwner {
+        claimPercentage = claimPercentage_;
     }
 
     function setClaimant(address claimant_) external onlyOwner {
@@ -38,26 +35,12 @@ contract Disbursement is Ownable {
             "Only debase policy contract can call this"
         );
 
-        if (claimAmount != 0) {
-            uint256 supply = debase.totalSupply();
-            if (supplyDelta_ < 0) {
-                supply = supply.sub(uint256(supplyDelta_.abs()));
-            } else if (supplyDelta_ > 0) {
-                supply = supply.add(uint256(supplyDelta_));
-            }
-
-            if (supply > MAX_SUPPLY) {
-                supply = MAX_SUPPLY;
-            }
-
-            uint256 claimShare = claimAmount.mul(10**18).div(supply);
+        if (claimPercentage != 0) {
             uint256 rewardToClaim =
-                debase.totalSupply().mul(claimShare).div(10**18);
+                debasePolicyBalance.mul(claimPercentage).div(10**18);
 
-            claimAmount = 0;
-            if (rewardToClaim <= debasePolicyBalance) {
-                return rewardToClaim;
-            }
+            claimPercentage = 0;
+            return rewardToClaim;
         }
 
         return 0;
